1a5d66da5a592ef6cb7e463064b8d88309276cc9
Travis Thompson <lamont00007@hotmail.com>
2023-03-10 11:56:23 -0500

    complete objective2lab3

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -3,7 +3,17 @@ public class Objective2Lab3 {
 
 
     /*
-      Your solution goes here
+    int year = 1982;
+    String month = "December";
+    y = "not a weekend";
+
+    System.out.println("int year");
+      System.out.println("string month");
+      System.out.println("y");
+
+      Expected Output: 1982 , Decemeber, not a weekend
+
+
     */
   }
 }
