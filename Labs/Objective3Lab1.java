5eaf9ee4009fe098ea0b06ccf2ba17a25c1644b8
Travis Thompson <lamont00007@hotmail.com>
2023-03-10 12:27:35 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,18 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    /*
+      int num1 = 15;
+      int num2 = 6;
+      int num3 = 2;
+
+      int num1 + int num2 + int num3 = int num4;
+
+      int num4 / 3 = 7.66666666667
+
+
+
+    */
+  }
+}
