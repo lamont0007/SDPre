238c300cc6f640302e14c04a4cae1dadefd2be9f
Travis Thompson <lamont00007@hotmail.com>
2023-03-10 11:38:28 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Travis Thompson";
+    int age = 40;
+    String state = "Virginia";
+
+    /*
+      Name: Travis Thompson Age: 40 State: Virginia
+    */
+  }
+}
