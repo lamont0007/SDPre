9c2c3e346ab96eb86385a7017d04db3a1d3fc2cb
Travvis Thompson <lamont00007@hotmail.com>
2023-04-18 16:07:22 -0400

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2 {
+        public static void main(String[] args) {
+		double num1,num2,hypotenuse;
+		num1 = 10;
+		num2 = 17;
+
+
+		hypotenuse = Math.sqrt(num1 * num1 + num2 * num2);
+		System.out.println(hypotenuse);
+
+
+	}
+}
