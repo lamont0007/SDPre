49c59cc4593ed0c0c951fc7cb21c2af677e5b7d5
Travvis Thompson <lamont00007@hotmail.com>
2023-04-18 12:14:58 -0400

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    
+      int birthyear = 1982;
+      String birthmonth = "December";
+      String Tuesday = "False";
+
+      System.out.println(birthyear);
+      System.out.println(birthmonth);
+      System.out.println(Tuesday);
+
+  }
+}
