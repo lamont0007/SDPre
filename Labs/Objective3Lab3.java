613941c75b8724c8747bba03a6cc4f39035eab4c
Travvis Thompson <lamont00007@hotmail.com>
2023-05-02 15:21:13 -0400

    finish objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,13 @@
+public class Objective3Lab3 {
+ public static void main(String[] args) {
+	
+
+	int first = 1 + (int)(Math.random()*6);
+	int second = 1 + (int)(Math.random()*6);
+	int third = 1 + (int)(Math.random()*6);
+
+		String equation = first + "+" + second + "+" + third + "=" + (first + second + third);
+
+	System.out.println(equation); 
+
+}}
