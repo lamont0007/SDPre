659f5f71981c693e82eb241441b21a8ef1105b17
Travvis Thompson <lamont00007@hotmail.com>
2023-05-04 21:18:06 -0400

    Complete Objective7Lab.java

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,23 @@
+public class Objective7Lab3 {
+	public static void main(String [] args) {
+	int counter = 0;
+
+
+	while (counter <= 20) {
+
+		if (counter % 2 == 0) {
+		System.out.println(counter + " is even");
+	}
+
+		else {
+			System.out.println(counter + " is odd");
+			
+		}
+
+
+		counter++;
+			}
+
+		}
+
+	}
