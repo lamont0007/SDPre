a5671a7e9b9a3f9e48074ea43dfa9a2793af3fea
Travvis Thompson <lamont00007@hotmail.com>
2023-05-05 14:27:39 -0400

    Objective8Lab3 Complete

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,23 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+
+for (int i=0; i<20; i+=1){
+ System.out.println(i);  
+}
+
+while (counter <= 20) {
+
+    if (counter % 2 == 0) {
+    System.out.println(counter + " is even");
+  }
+
+    else {
+      System.out.println(counter + " is odd");
+      
+    }
+    
+  }
+// this one has been working but not allowing the counter to add my numbers//
+}}
